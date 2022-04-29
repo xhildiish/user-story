d04654bd02395090162e992000aa09231308bfe3
Xhildish <55058392+xhildiish@users.noreply.github.com>
2022-04-29 15:19:30 +0200

    Update keys.ts

--- backend/src/config/keys.ts
+++ backend/src/config/keys.ts
@@ -1,6 +1,6 @@
 export default {
-    githubClientID: '98ca361010dcb1a7f2d2' ,
-    githubClientSecret:'659b00a07a42085f8f7b1be29c9e9b7bb19457a7',
+    githubClientID: '' ,
+    githubClientSecret:'',
     //mongoURI:'mongodb://localhost:27017/user-story' // LINUX
     mongoURI:'mongodb://userstory-mongo:27017/user-story' // DOCKER
     //mongoURI:'mongodb://127.0.0.1:27017/user-story' // WINDOWS
